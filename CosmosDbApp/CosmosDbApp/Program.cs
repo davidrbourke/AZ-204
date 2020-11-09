401914d71acb129d617588d448d6441e02d10a51
David Bourke <davidrbourke@gmail.com>
2020-11-09 21:34:12 +0000

    Addition some initial code, inc. cosmosdb example

---
+++ CosmosDbApp/CosmosDbApp/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Cosmos;
+
+namespace CosmosDbApp
+{
+    class Program
+    {
+        // The Azure Cosmos DB endpoint for running this sample.
+        private static readonly string EndpointUri = "";
+        // The primary key for the Azure Cosmos account.
+        private static readonly string PrimaryKey = "";
+
+        // The Cosmos client instance
+        private CosmosClient cosmosClient;
+
+        // The database we will create
+        private Database database;
+
+        // The container we will create.
+        private Container container;
+
+        // The name of the database and container we will create
+        private string databaseId = "FamilyDatabase";
+        private string containerId = "FamilyContainer";
+
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Beginning operations...\n");
+                Program p = new Program();
+                await p.GetStartedDemoAsync();
+            }
+            catch (CosmosException de)
+            {
+                Exception baseException = de.GetBaseException();
+                Console.WriteLine($"{de.StatusCode} error occurred: {de}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e}");
+            }
+            finally
+            {
+                Console.WriteLine("End of demo");
+                Console.ReadKey();
+            }
+        }
+
+        /*
+            Entry point to call methods that operate on Azure Cosmos DB resources in this sample
+        */
+        public async Task GetStartedDemoAsync()
+        {
+            ValidateConnectionDetails();
+            // Create a new instance of the Cosmos Client
+            this.cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+            await this.CreateDatabaseAsync();
+            await this.CreateContainerAsync();
+            await this.AddItemsToContainerAsync();
+            await this.QueryItemsAsync();
+            await this.ReplaceFamilyItemAsync();
+            await this.DeleteFamilyItemAsync();
+            await this.DeleteDatabaseAndCleanupAsync();
+        }
+
+        private void ValidateConnectionDetails()
+        {
+            if (string.IsNullOrWhiteSpace(EndpointUri))
+                throw new ArgumentNullException(nameof(EndpointUri));
+
+            if (string.IsNullOrWhiteSpace(PrimaryKey))
+                throw new ArgumentNullException(nameof(PrimaryKey));
+        }
+
+        public async Task CreateDatabaseAsync()
+        {
+            this.database = await this.cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
+            Console.WriteLine($"Created database: {this.databaseId}\n");
+        }
+
+        private async Task CreateContainerAsync()
+        {
+            this.container = await this.database.CreateContainerIfNotExistsAsync(containerId, "/LastName");
+            Console.WriteLine($"Created container: {this.containerId}\n");
+        }
+
+        private async Task AddItemsToContainerAsync()
+        {
+            Family andersenFamily = new Family
+            {
+                Id = "Andersen.1",
+                LastName = "Andersen",
+                Parents = new Parent[]
+                {
+                    new Parent { FirstName = "Thomas" },
+                    new Parent { FirstName = "Mary Kay" }
+                },
+                Children = new Child[]
+                {
+                    new Child
+                    {
+                        FirstName = "Henriette Thaulow",
+                        Gender = "female",
+                        Grade = 5,
+                        Pets = new Pet[]
+                        {
+                            new Pet { GivenName = "Fluffy" }
+                        }
+                    }
+                },
+                Address = new Address { State = "WA", County = "King", City = "Seattle" },
+                IsRegistered = false
+            };
+
+            try
+            {
+                ItemResponse<Family> andersenFamilyResponse = await this.container.ReadItemAsync<Family>(andersenFamily.Id, new PartitionKey(andersenFamily.LastName));
+                Console.WriteLine($"Item in database with id: {andersenFamilyResponse.Resource.Id} already exists\n");
+            }
+            catch(CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+            {
+                ItemResponse<Family> andersenFamilyResponse = await this.container.CreateItemAsync<Family>(andersenFamily, new PartitionKey(andersenFamily.LastName));
+                Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", andersenFamilyResponse.Resource.Id, andersenFamilyResponse.RequestCharge);
+            }
+        }
+
+        private async Task QueryItemsAsync()
+        {
+            var sqlQueryText = "SELECT * FROM c WHERE c.LastName ='Andersen'";
+
+            Console.WriteLine("Running query: {0}\n", sqlQueryText);
+
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
+            FeedIterator<Family> queryResultSetIterator = this.container.GetItemQueryIterator<Family>(queryDefinition);
+
+            List<Family> families = new List<Family>();
+
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<Family> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach(Family family in currentResultSet)
+                {
+                    families.Add(family);
+                    Console.WriteLine($"\tRead {family}\n");
+                }
+            }
+        }
+
+        private async Task ReplaceFamilyItemAsync()
+        {
+            ItemResponse<Family> andersenFamilyResponse = await this.container.ReadItemAsync<Family>("Andersen.1", new PartitionKey("Andersen"));
+            Family itemBody = andersenFamilyResponse.Resource;
+            itemBody.IsRegistered = true;
+            itemBody.Children[0].Grade = 100;
+
+            andersenFamilyResponse = await this.container.ReplaceItemAsync<Family>(itemBody, itemBody.Id, new PartitionKey(itemBody.LastName));
+            Console.WriteLine("Updated Family [{0},{1}].\n \tBody is now: {2}\n", itemBody.LastName, itemBody.Id, andersenFamilyResponse.Resource);
+        }
+
+        private async Task DeleteFamilyItemAsync()
+        {
+            var partitionKeyValue = "Andersen";
+            var familyId = "Andersen.1";
+
+            ItemResponse<Family> andersenFamilyResponse = await this.container.DeleteItemAsync<Family>(familyId, new PartitionKey(partitionKeyValue));
+            Console.WriteLine("Deleted Family [{0},{1}]\n", partitionKeyValue, familyId);
+        }
+
+        private async Task DeleteDatabaseAndCleanupAsync()
+        {
+            DatabaseResponse databaseResponseResource = await this.database.DeleteAsync();
+            Console.WriteLine($"Deleted database {this.databaseId}");
+            this.cosmosClient.Dispose();
+        }
+    }
+}
+
--- README.md
+++
@@ -1,3 +0,0 @@
-# AZ-204
-
-## Code and infrastructure samples and examples for AZ-204 Exam.
