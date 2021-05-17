8040fb3be07714cfe6cf49e3507a74d2873ffc2d
David Bourke <davidrbourke@gmail.com>
2021-05-17 20:23:41 +0100

    Adding key vault access, service principal and managed identity demos

---
+++ KeyVaultDemoApp/KeyVaultDemoApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Keys;
+using Azure.Security.KeyVault.Keys.Cryptography;
+using Azure.Security.KeyVault.Secrets;
+using System;
+using System.Text;
+
+namespace KeyVaultDemoApp
+{
+    /// <summary>
+    /// Demos reading from KeyVault using Service Principal
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Read from keyvault");
+
+            // Setup in Azure:
+            // -> Create a Service Principal in Azure AD using Azure CLI
+            // az ad sp create-for-rbac -n {keyvaultname} --skip-assignemtn
+            // The above generates a result that should be set an environment variables
+            // appId = AZURE_CLIENT_ID
+            // password = AZURE_CLIENT_SECRET
+            // tenant = AZURE_TENANT_ID
+
+            // Set access policy on KeyVault
+            // az keyvault set-policy --name {keyvaultname} --spn "{appId from above}" --secret-permissions get list delete (etc, etc)
+            var keyVaultUrl = "https://trainingvaultdrb.vault.azure.net/";
+
+            // DefaultAzureCredentials will get the settings from above from the environment variables
+            var secretClient = new SecretClient(vaultUri: new Uri(keyVaultUrl), credential: new DefaultAzureCredential());
+
+            // Get a Secret
+            KeyVaultSecret secret = secretClient.GetSecret("apppwd");
+
+            Console.WriteLine($"{secret.Name}, {secret.Value}");
+
+            // Get a Key
+            var keyClient = new KeyClient(vaultUri: new Uri(keyVaultUrl), credential: new DefaultAzureCredential());
+
+            KeyVaultKey key = keyClient.GetKey("drbenckey");
+
+            // Encrypts using the key - these are Azure libraries
+            var cyptoClient = new CryptographyClient(key.Id, credential: new DefaultAzureCredential());
+            byte[] plaintext = Encoding.UTF8.GetBytes("text to encrypt");
+            EncryptResult result = cyptoClient.Encrypt(EncryptionAlgorithm.RsaOaep256, plaintext);
+
+            Console.WriteLine(Encoding.UTF8.GetString(result.Ciphertext));
+        }
+    }
+}
---
+++ WebAppManagedIdentity/WebAppManagedIdentity/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using WebAppManagedIdentity.Models;
+
+namespace WebAppManagedIdentity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        private string _secretIdentifier = "https://trainingvaultdrb.vault.azure.net/secrets/apppwd/4601676f4944499a9d21914f792ef4bd";
+
+        public async Task<IActionResult> Index()
+        {
+            // Create Token Provider - will use Identity on Resource to generate a token
+            // Setup:
+            // In Azure portal
+            // -> App Service
+            // -> Identity
+            // -> set System Assigned Identity: On
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            
+            // Connect to keyvault with Managed Identity generated token
+            // Setup:
+            // In Azure portal
+            // -> Key Vault
+            // -> Access Policies
+            // -> Add Access Policy
+            // -> Select Principal (search for the App Identity)
+            // -> Add Permissions and Save
+            var secretClient = new KeyVaultClient
+                (new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+            // Access Secret
+            var secret = await secretClient.GetSecretAsync(_secretIdentifier)
+                .ConfigureAwait(false);
+
+            ViewBag.Sec = secret.Value;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
