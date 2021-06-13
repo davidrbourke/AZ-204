f7d7f1aa2d4238159ce17ec30c3755daad55d534
David Bourke <davidrbourke@gmail.com>
2021-06-13 17:26:31 +0100

    Added storage account queue demo

---
+++ StorageAccountQueueApp/StorageAccountQueueApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
+using System;
+using System.Threading.Tasks;
+
+namespace StorageAccountQueueApp
+{
+    public class Program
+    {
+        private static string _connStr = "";
+        public static async Task Main(string[] args)
+        {
+            CloudStorageAccount account = CloudStorageAccount.Parse(_connStr);
+
+            CloudQueueClient client = account.CreateCloudQueueClient();
+
+            CloudQueue cloudQueue = client.GetQueueReference("demosample");
+
+            CloudQueueMessage message = new CloudQueueMessage($"this is message on the queue {DateTime.Now}");
+
+            await cloudQueue.AddMessageAsync(message);
+
+            Console.WriteLine("Message added to queue");
+
+
+            // Read from Queue
+            await cloudQueue.FetchAttributesAsync();
+            if (cloudQueue.ApproximateMessageCount > 0)
+            {
+                CloudQueueMessage dequeueMessage = await cloudQueue.GetMessageAsync();
+
+                Console.WriteLine("Message downloaded - reads:)");
+                Console.WriteLine(dequeueMessage.AsString);
+
+                await cloudQueue.DeleteMessageAsync(dequeueMessage);
+            }
+        }
+    }
+}
