bed589f8a42bb8db4e86600cf3e36744e55e84be
David Bourke <davidrbourke@gmail.com>
2021-06-16 20:46:26 +0100

    Code comments added for service bus demo

--- AzureServiceBusPublisher/AzureServiceBusPublisher/Program.cs
+++ AzureServiceBusPublisher/AzureServiceBusPublisher/Program.cs
@@ -21,6 +21,10 @@ namespace AzureServiceBusPublisher
             }
 
             Console.WriteLine("Service Bus Publisher");
+            
+            // A Shared Access Policy must be created on the Service Bus Namespace or on the Queue.
+            // Once the connection string is available on the policy in Azure, copy it here and remove the entity part at the end.
+            // Aslo set the queue name in queueEntityName.
             var connStr = "";
             var queueEntityName = "";
             queueClient = new QueueClient(connStr, queueEntityName);
@@ -44,6 +48,7 @@ namespace AzureServiceBusPublisher
         {
             for (int i = 0; i < 100; i++)
             {
+                // Messages can only be written as bytes.
                 var message = new Message(Encoding.UTF8.GetBytes($"Message {i}"));
                 await queueClient.SendAsync(message);
             }
@@ -53,24 +58,32 @@ namespace AzureServiceBusPublisher
 
         static void RunAsSubscriber()
         {
+            // Configure message options
+            // The exception handler delegate must be passed in to the constructor.
             var options = new MessageHandlerOptions(HandleException)
             {
                 MaxConcurrentCalls = 1,
                 AutoComplete = false
             };
 
+            // Register the message handler, passing it in as a delegate
             queueClient.RegisterMessageHandler(HandleMessage, options);
 
             Console.ReadKey();
         }
 
+        // This is the actual message handler that will process each message
         static async Task HandleMessage(Message msg, CancellationToken token)
         {
+            // Decode from bytes to string
             var msgTxt = Encoding.UTF8.GetString(msg.Body);
             Console.WriteLine($"Message received: {msgTxt}");
+
+            // As the AutoComplete option is set to false, the message must be exlicitly set to completed.
             await queueClient.CompleteAsync(msg.SystemProperties.LockToken);
         }
 
+        // The method for handling exceptions.
         static Task HandleException(ExceptionReceivedEventArgs args)
         {
             Console.WriteLine(args.Exception);
