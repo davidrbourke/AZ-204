7da70c4fc2eb7b1417accee597d42a9dbac1c954
David Bourke <davidrbourke@gmail.com>
2021-09-21 08:56:42 +0100

    Adding graph demo from Console app

---
+++ GraphDemoConsoleApp/GraphDemoConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Identity.Client;
+using Microsoft.Graph;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Graph.Auth;
+
+namespace GraphDemoConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var clientId = "";
+            var scopes = new[] { "User.Read" };
+            
+            IPublicClientApplication publicClientApplication = PublicClientApplicationBuilder
+                        .Create(clientId)
+                        .Build();
+
+
+            DeviceCodeProvider authProvider = new DeviceCodeProvider(publicClientApplication, scopes);
+            var graphClient = new GraphServiceClient(authProvider);
+
+            var me = await graphClient.Me.Request().GetAsync();
+
+           
+        }
+    }
+}
