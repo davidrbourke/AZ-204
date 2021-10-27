7a13b9f2048f40b6756b8debc05f8dc29f75fd7b
David Bourke <davidrbourke@gmail.com>
2021-10-27 08:32:27 +0100

    Key vault app example - added application insights

---
+++ KeyVaultMVCApp/KeyVaultMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using KeyVaultMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace KeyVaultMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var keyVaultUri = "https://az204keyvaultdrb.vault.azure.net";
+            SecretClient client = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential());
+
+            var secret = await client.GetSecretAsync("SqlPwd");
+
+            ViewBag.SqlPwd = secret.Value.Value;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
