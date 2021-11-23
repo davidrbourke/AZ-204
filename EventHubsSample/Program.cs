ecfc7c0b1abe123f79337cbd736d9f303d754610
David Bourke <davidrbourke@gmail.com>
2021-11-23 08:48:15 +0000

    Event hub consumer code added

--- EventHubsSample/Program.cs
+++ EventHubsSample/Program.cs
@@ -1,11 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
 using Azure.Messaging.EventHubs.Producer;
 
 
 var client = new ClientManager();
 
-client.SendMessagesToEventHub().Wait();
+//client.SendMessagesToEventHub().Wait();
+client.RetrieveMessagesFromEventHub().Wait();
 
 public class ClientManager
 {
@@ -20,6 +22,7 @@ public class ClientManager
 
         try
         {
+            // Using a event data batch object validates message size <= 1MB
             using EventDataBatch eventBatch = await producer.CreateBatchAsync();
 
             for (var i = 0; i < 10; i++)
@@ -43,8 +46,79 @@ public class ClientManager
         Console.WriteLine("Completed writing events");
     }
 
-    public void RetrieveMessagesFromEventHub()
+    public async Task SendMessagesToEventHubExplicitPartitionKey()
     {
+        Console.WriteLine("Start writing events");
 
+        var producer = new EventHubProducerClient(_connectionString, _eventHubName);
+
+        try
+        {
+            // Using a event list like this doesn't not validate message size <= 1MB
+            var events = new List<EventData>();
+            
+            for (var i = 0; i < 10; i++)
+            {
+                var eventData = new EventData($"Event #{i}");
+
+                events.Add(eventData);
+            }
+            var options = new SendEventOptions();
+
+            /************************************************************/
+            // Events with same partition key are writen to same partition
+            options.PartitionKey = "p1";
+            /************************************************************/
+
+            Console.WriteLine("Sending events to event hub");
+            await producer.SendAsync(events, options);
+        }
+        finally
+        {
+            await producer.CloseAsync();
+        }
+
+        Console.WriteLine("Completed writing events");
+    }
+
+    public async Task RetrieveMessagesFromEventHub()
+    {
+        var consumer = new EventHubConsumerClient(
+            EventHubConsumerClient.DefaultConsumerGroupName,
+            _connectionString,
+            _eventHubName);
+
+        try
+        {
+
+
+            using var cancellationSource = new CancellationTokenSource();
+            cancellationSource.CancelAfter(TimeSpan.FromSeconds(90));
+
+            var maximumEvents = 100;
+            var eventDataRead = new List<string>();
+
+            await foreach (PartitionEvent partitionEvent in consumer.ReadEventsAsync(cancellationSource.Token))
+            {
+
+                var eventMessage = partitionEvent.Data.EventBody.ToString();
+                eventDataRead.Add(eventMessage);
+
+                Console.WriteLine(eventMessage);
+
+                if (eventDataRead.Count > maximumEvents)
+                {
+                    break;
+                }
+            }
+        }
+        catch(Exception ex)
+        {
+            Console.Error.WriteLine(ex.ToString());
+        }
+        finally
+        {
+            await consumer.CloseAsync();
+        }
     }
 }
